63263894e4a5aad3058c7ff7d60d8eeab14199a7
Ivan Cvetomirov Ivanov <slayer_word_11@hotmail.com>
2014-11-28 17:00:11 +0200

    start  to ver 2.0 ramones
    start  to ver 2.0 ramones

---
+++ ramones-rc2/content.php
@@ -0,0 +1,18 @@
+	<article class="post" id="post-<?php the_ID(); ?>">
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<h2 class="post-header">				
+				<a href="<?php the_permalink(); ?>" title="<?php sprintf( __( 'Permanent Link to %s', 'ramones' ), the_title_attribute( 'echo=0' ) ); ?>">
+					<?php the_title(); ?>
+				</a>
+			</h2>
+			<div class="post-content">				
+				<?php the_content();?>				
+			</div>
+			<div class="post-date">
+				<?php the_date();?>
+			</div>
+			<div class="post-category">
+				<?php the_category();?>
+			</div>		
+		</div>		
+	</article>
---
+++ ramones-rc2/sidebar-footer.php
@@ -0,0 +1,5 @@
+<?php
+	if(is_active_sidebar('sidebar-footer')){			
+		dynamic_sidebar('sidebar-footer');
+	}	
+?>
