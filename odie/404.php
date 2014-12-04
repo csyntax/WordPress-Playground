b58f4ecfa5afd0a0bd0c1bd45b64f8b2c37007f3
Ivan Cvetomirov Ivanov <slayer_word_11@hotmail.com>
2014-12-04 20:25:00 +0200

    add odie theme and github plugin
    add odie theme and github plugin

---
+++ odie/404.php
@@ -0,0 +1,26 @@
+﻿<?php get_header(); ?>	
+	<div class="container">
+		<div class="row">             
+			<div class="col-md-4">				
+				<?php 
+					get_search_form();
+					get_sidebar('main');
+                 ?>
+			</div>
+			<div class="col-xs-12 col-md-8">	
+				<h3 class="post-header">				
+					<?php 
+						_e('Нищо не намери','odie');
+					?>	
+				</h3>
+				<div class="posts">
+					<div class="post-content">				
+						<?php 
+							_e( 'Няма такaва страница', 'odie' ); 
+						?>
+					</div>
+				</div>				
+			</div>					
+		</div>	
+	</div>
+<?php get_footer(); ?>
---
+++ odie/single.php
@@ -0,0 +1,38 @@
+﻿<?php get_header(); ?>	
+	<div class="container">
+		<div class="row">             
+			<div class="col-md-4">				
+				<?php 
+					get_search_form();				
+                 	get_sidebar('main');
+                 ?>
+			</div>
+            <div class="col-xs-12 col-md-8">                				
+				<div class="posts">
+					<?php 
+						if(have_posts()):
+							while(have_posts()):
+								the_post();				
+								get_template_part("content");
+					?>
+					<ul class="pager">
+						<li class="previous">  
+							<?php 
+								previous_post_link('%link','<span class="previous-post">'._x( '&larr;','Предишен пост','odie').'%title</span>');
+							?>
+						</li>
+						<li class="next">
+							<?php 
+								next_post_link('%link','<span class="next-post">%title'._x('&rarr;', 'Следващ пост', 'odie').'</span>');
+							?>
+						</li>
+					</ul>
+					<?php 
+							endwhile;		
+						endif;
+					?>		
+				</div>            
+			</div>
+		</div>
+    </div>
+<?php get_footer(); ?>
